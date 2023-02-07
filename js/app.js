c9b55d00c9bd7d547c8476777bd9b4be346d0ba9
DmytroKuts <40741910+Dmytro24@users.noreply.github.com>
2023-02-07 05:11:40 +0200

    Updates

--- js/app.js
+++ js/app.js
@@ -744,7 +744,33 @@
     };
     const da = new DynamicAdapt("max");
     da.init();
-    window["FLS"] = true;
+    window.addEventListener("load", windowLoad);
+    function windowLoad() {
+        const htmlBlock = document.documentElement;
+        const saveUserTheme = localStorage.getItem("user-theme");
+        let userTheme;
+        if (window.matchMedia) userTheme = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+        window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", (e => {
+            !saveUserTheme ? changeTheme() : null;
+        }));
+        const themeButton = document.querySelector(".theme");
+        if (themeButton) themeButton.addEventListener("click", (function(e) {
+            changeTheme(true);
+        }));
+        function setThemeClass() {
+            if (saveUserTheme) htmlBlock.classList.add(saveUserTheme); else htmlBlock.classList.add(userTheme);
+        }
+        setThemeClass();
+        function changeTheme(saveTheme = false) {
+            let currentTheme = htmlBlock.classList.contains("light") ? "light" : "dark";
+            let newTheme;
+            if ("light" === currentTheme) newTheme = "dark"; else if ("dark" === currentTheme) newTheme = "light";
+            htmlBlock.classList.remove(currentTheme);
+            htmlBlock.classList.add(newTheme);
+            saveTheme ? localStorage.setItem("user-theme", newTheme) : null;
+        }
+    }
+    window["FLS"] = false;
     isWebp();
     menuInit();
     spollers();
